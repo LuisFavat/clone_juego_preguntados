0daa2d2dda7430967415b6d9b8b5120e07a8aa77
LuisFavat <73365264+LuisFavat@users.noreply.github.com>
2022-11-21 14:52:44 -0300

    switch pc

--- preguntados/src/Pages/Play/Play.js
+++ preguntados/src/Pages/Play/Play.js
@@ -2,7 +2,7 @@ import Question from "../../components/Question/Question";
 import "./Play.css"
 import Api from "../../Api/preguntados"
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Button from "../../components/Button/Button";
 
 
@@ -11,39 +11,41 @@ const Play = () => {
 
     const [questionsLoaded, setQuestionsLoaded] = useState([[],false])
 
-    let {aDificulty} = useParams()
-
-    console.log("Antes", questionsLoaded)
+    let {dificulty} = useParams()
+    console.log("a dificulty: ", dificulty)
 
     useEffect(() => {
-        Api.getQuestions(aDificulty)
+        Api.getQuestions(dificulty)
             .then((data) => {
                 setQuestionsLoaded([data, true])
-                console.log("here iam", questionsLoaded.at(1))
     })
             .catch((error) => error)
     },[])
  
     const [actualQuestion, setActualQuestion] = useState(0)
 
+    const navigate = useNavigate()
+
     const submitAnswer = () => {
        setActualQuestion(actualQuestion + 1)
+       navigate(`/play/${dificulty}?question=${actualQuestion+1}`)
     }
 
     return(
         <div className="PlayQuestionWrapper">
             { questionsLoaded.at(1)?
                 <Question 
+                    submit={submitAnswer}
                     questionID={ questionsLoaded.at(0)[actualQuestion].id }
-                    question={questionsLoaded.at(0)[actualQuestion].question} 
-                    option1={questionsLoaded.at(0)[actualQuestion].option1} 
-                    option2={questionsLoaded.at(0)[actualQuestion].option2} 
-                    option3={questionsLoaded.at(0)[actualQuestion].option3} 
-                    option4={questionsLoaded.at(0)[actualQuestion].option4}
+                    question={ questionsLoaded.at(0)[actualQuestion].question } 
+                    option1={ questionsLoaded.at(0)[actualQuestion].option1 } 
+                    option2={ questionsLoaded.at(0)[actualQuestion].option2 } 
+                    option3={ questionsLoaded.at(0)[actualQuestion].option3 } 
+                    option4={ questionsLoaded.at(0)[actualQuestion].option4 }
                     />
              : <div/>}
              <div>
-                <Button action={submitAnswer} tag ={"Next question"}/>
+                <Button action={ submitAnswer } tag ={"Next question"}/>
              </div>
         </div>
     )
--- preguntados/src/components/Question/Question.js
+++ preguntados/src/components/Question/Question.js
@@ -1,33 +1,42 @@
+import { useEffect, useState } from "react"
 import Api from "../../Api/preguntados"
 import "./Question.css"
+import Button from "../Button/Button"
 
-const Question = ({questionID, question, option1, option2, option3, option4}) =>
+const Question = ({action, questionID, question, option1, option2, option3, option4}) =>
 {
-    console.log("question ID", questionID)
+   
+    const [questionsState, setQuestionsState] = useState(["unCheck", "unCheck", "unCheck", "unCheck"])
 
+    const unCkeckState = () => Array(4).fill("unCheck")
 
-    const Option = ({id, aOption}) => { 
+    const Option = ({style, id, aOption}) => { 
 
         const sendOption = () => {
             Api.postOption(questionID, `option${id}`)
-                .then((data) => console.log(data) )
+                .then((data) => {
+                        let newState = unCkeckState()
+                        newState.splice(id -1, 1, data.answer.toString())
+                        console.log("VEr el new stae", newState)
+                        setQuestionsState(newState)
+                    })
                 .catch(function (error){
                     console.log("ERROR!!!", error)
                 })            
         }
 
         return(
-            <div onClick={sendOption}>{ aOption }</div>
+            <div className={style} onClick={sendOption}>{ aOption }</div>
         )
     }
 
     return(
-        <div className="QuestionWrapper">
+        <div className="QuestionWrapper" >
             <div>{ question }</div>
-            <Option  id={1} aOption={option1}/>
-            <Option  id={2} aOption={option2}/>
-            <Option  id={3} aOption={option3}/>
-            <Option  id={4} aOption={option4}/>
+            <Option style={questionsState[0]} id={1}  aOption={option1}/>
+            <Option style={questionsState[1]} id={2}  aOption={option2}/>
+            <Option style={questionsState[2]} id={3}  aOption={option3}/>
+            <Option style={questionsState[3]} id={4}  aOption={option4}/>
         </div>
     )
 }
